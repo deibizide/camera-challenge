92852ea2684c2714fa86792564f1074ea1405645
deibizide <david.ramis89@gmail.com>
2020-10-15 20:09:18 +0200

    Updates

---
+++ precache-manifest.a6745d2343a16c543f57e197f8cb4dcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4b8202476d8d6da66e7e80d389f791",
+    "url": "/camera-challenge/index.html"
+  },
+  {
+    "revision": "311ec958ef8c8e8e69ff",
+    "url": "/camera-challenge/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "efc1d7eb45e065a92381",
+    "url": "/camera-challenge/static/css/main.55878a29.chunk.css"
+  },
+  {
+    "revision": "311ec958ef8c8e8e69ff",
+    "url": "/camera-challenge/static/js/2.e95e07e6.chunk.js"
+  },
+  {
+    "revision": "a7aec927f1e863a53de22e58e5329447",
+    "url": "/camera-challenge/static/js/2.e95e07e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc1d7eb45e065a92381",
+    "url": "/camera-challenge/static/js/main.cb3f1d95.chunk.js"
+  },
+  {
+    "revision": "cdc9fde38ee1445c4fe7",
+    "url": "/camera-challenge/static/js/runtime-main.040175ac.js"
+  }
+]);

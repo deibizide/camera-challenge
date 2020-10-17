8ae03e703018bd2021cf202dace89375e7abf8c5
deibizide <david.ramis89@gmail.com>
2020-10-17 10:54:33 +0200

    create delete/send button

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,6 +5,9 @@ import Button from 'react-bootstrap/Button';
 // import useImagePreview from '../../hooks/usePreviewImage';
 // style
 import './style.scss';
+// font-awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 interface Props {
     isUserReady: boolean;
@@ -14,18 +17,48 @@ interface Props {
 const Home: React.FC<Props> = () => {
     // const { getPreview } = useImagePreview();
     const [file, setFile] = useState('');
+    const [hasPreview, setHasPreview] = useState(false);
 
-    const getImagePreview = e => {
+    const getImagePreview = (e): void => {
         setFile(URL.createObjectURL(e.target.files[0]));
+        setHasPreview(true);
+    };
+
+    const reTakePhoto = (): void => {
+        setFile('');
+        setHasPreview(false);
+    };
+
+    const sendFile = (): void => {
+        console.log('I am sending the file');
     };
 
     return (
         <div className="home d-flex justify-content-center align-items-center flex-column">
-            <h5 className="text-center pb-5">Do you want to send an image to your landlord?</h5>
-            <Button variant="outline-light" className="btn btn-outline-secondary d-flex p-0">
-                <input type="file" accept="image/*" id="file" onChange={(e): void => getImagePreview(e)} />
-            </Button>
-            <img src={file} width="250" height="250" alt="Preview of the upload" />
+            {!hasPreview && (
+                <>
+                    <h5 className="text-center pb-5">Do you want to send an image to your landlord?</h5>
+                    <Button variant="outline-light" className="btn btn-outline-secondary d-flex p-0">
+                        <input type="file" accept="image/*" id="file" onChange={(e): void => getImagePreview(e)} />
+                    </Button>
+                </>
+            )}
+            {hasPreview && (
+                <>
+                    <img src={file} width="250" height="250" alt="Preview of the upload" />
+
+                    <div className="text-center d-flex ">
+                        <Button variant="warning" className="btn d-flex align-items-center mx-3" onClick={reTakePhoto}>
+                            <p className="m-0 px-3">Delete</p>
+                            <FontAwesomeIcon icon={faTrashAlt} />
+                        </Button>
+                        <Button variant="info" className="btn d-flex align-items-center mx-3" onClick={sendFile}>
+                            <p className="m-0 px-3">Send</p>
+                            <FontAwesomeIcon icon={faPaperPlane} />
+                        </Button>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
